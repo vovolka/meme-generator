8fa4db3d0a8f7e55fac0202b296b81b558e8cb3c
vovolka <alexander.vovolka@gmail.com>
2015-02-13 13:34:00 +0200

    add textListener to top and bottom text fields

--- app.js
+++ app.js
@@ -1,27 +1,56 @@
+window.imgSrc = null;
 
+
+function textListener (event) {
+    var id = event.srcElement.id;
+    var text = event.srcElement.value;
+    if(id == 'topText'){
+        window.topText = text;
+        console.log(window.topText)
+    }
+    else {
+        window.bottomText = text;
+    }
+    drawMeme(window.imgSrc, window.topText, window.bottomText);
+}
+
+function drawMeme(image, topText, bottomText){
+    var canvas = document.querySelector('canvas');
+    var ctx = canvas.getContext("2d");
+    canvas.width = 500;
+    canvas.height = 500;
+
+    if(image !=null){
+        ctx.drawImage(image,0,0, canvas.width, canvas.height);
+    }
+
+    ctx.font = "42px Impact";
+    ctx.textAlign = "center";
+    ctx.fillStyle = "#ffffff";
+    ctx.strokeStyle = "#000000";
+    ctx.lineWidth = 2;
+
+    //topLine
+    if(topText !=null){
+        ctx.fillText(topText, canvas.width/2,canvas.height -450);
+        ctx.strokeText(topText, canvas.width/2,canvas.height -450);
+    }
+    //bottomLine
+    if(bottomText !=null){
+        ctx.fillText(bottomText,canvas.width/2,canvas.height -50);
+        ctx.strokeText(bottomText,canvas.width/2,canvas.height -50);
+    }
+}
+
+function fileSelect(event){
+    console.log(event);
+}
 
 window.topText ="";
 window.bottomText = "";
 var input1 = document.getElementById('topText');
 var input2 = document.getElementById('bottomText');
-console.log(input1);
-
 input1.oninput = textListener;
 input2.oninput = textListener;
-console.log(input1);
 
-function textListener (event) {
-    console.log(event);
-}
-
-function drawMeme(){
-    var canvas = document.querySelector('canvas');
-    var ctx = canvas.getContext("2d");
-
-    //topLine
-    ctx.fillText();
-    ctx.strokeText();
-    //buttomLine
-    ctx.fillText();
-    ctx.strokeText();
-}
+document.getElementById('file').addEventListener('change',fileSelect,false);
