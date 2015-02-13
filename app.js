4caca2c15b4c0684dafce9de7ddfd7c339d932c7
vovolka <alexander.vovolka@gmail.com>
2015-02-13 11:26:48 +0200

    initial commit

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+
+window.topText ="";
+window.bottomText = "";
+var input1 = document.getElementById('topText');
+var input2 = document.getElementById('bottomText');
+console.log(input1);
+
+input1.oninput = textListener;
+input2.oninput = textListener;
+console.log(input1);
+
+function textListener (event) {
+    console.log(event);
+}
+
+function drawMeme(){
+    var canvas = document.querySelector('canvas');
+    var ctx = canvas.getContext("2d");
+
+    //topLine
+    ctx.fillText();
+    ctx.strokeText();
+    //buttomLine
+    ctx.fillText();
+    ctx.strokeText();
+}
